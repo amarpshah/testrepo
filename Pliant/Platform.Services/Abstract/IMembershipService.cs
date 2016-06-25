1fd9529eda9af54b91690fb2213a93db0235049a
Yogeshmali000 <yogeshmali000@gmail.com>
2016-06-25 07:46:07 +0000

    Adding Service Project

---
+++ Pliant/Platform.Services/Abstract/IMembershipService.cs
@@ -0,0 +1,19 @@
+﻿using Institute.Entities;
+using Institute.Services.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Institute.Services
+{
+    public interface IMembershipService
+    {
+        MembershipContext ValidateUser(string username, string password);
+        User CreateUser(string username, string email, string password, int[] roles);
+        User UpdateUser(int id, string username, string email, string password, int[] roles);
+        User GetUser(int userId);
+        List<Role> GetUserRoles(string username);
+    }
+}
